d3b7320ca38fade7909c23aa8f3dd8307fabd2ab
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-11-09 10:42:58 -0300

    Global styling and Starting navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <p>Crypterodactyl</p>
+    <div className='App'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'></div>
+      <div className='footer'></div>
     </div>
   );
 }
