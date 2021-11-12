7f8da320c0ed6691945726196786d024e719d555
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-11-12 16:13:16 -0300

    News section

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,18 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const cryptoApiHeaders = {
+  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_HOST,
+  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
+}
 
-// const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const baseUrl = process.env.REACT_APP_CRYPTOS_URL;
 
-// const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+console.log(baseUrl)
 
-// export const cryptoAPI = createApi({
-//   reducerPath: 'cryptoAPI',
-//   baseQuery: fetchBaseQuery({ baseUrl }),
-//   endpoints: (builder) => ({
-//     getCryptos: builder.query({
-//       query: (count) => createRequest(`/coins?limit=${count}`),
-//     }),
-//   }),
-// });
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-// export const { useGetCryptosQuery } = cryptoAPI;
+export const cryptoAPI = createApi({
+  reducerPath: 'cryptoAPI',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoAPI;
