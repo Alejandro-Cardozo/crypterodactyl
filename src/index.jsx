934537161b19523316104956ca08679eda57a798
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-11-12 10:18:36 -0300

    Using RTK query to create an API

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
 
+import store from './app/store';
 import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+
+// const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+// const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+// export const cryptoAPI = createApi({
+//   reducerPath: 'cryptoAPI',
+//   baseQuery: fetchBaseQuery({ baseUrl }),
+//   endpoints: (builder) => ({
+//     getCryptos: builder.query({
+//       query: () => createRequest('/coins'),
+//     }),
+//   }),
+// });
+
+// export const { useGetCryptosQuery } = cryptoAPI;
